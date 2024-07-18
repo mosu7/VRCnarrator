1d101653e34754efa3790b2cb7705a263d242677
mosu7 <nhdu73@gmail.com>
2024-07-19 02:44:21 +0800

    Udon Graph Talking Bubble

---
+++ Assets/Scripts/AI/GPTHandler.cs
@@ -0,0 +1,24 @@
+﻿using UdonSharp;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class GPTHandler : UdonSharpBehaviour
+{
+
+    // privat Button submitButton;
+    // public GameObject target;
+
+    void Start()
+    {
+        // Since we can't use AddListener, we will call a method on button click
+        // submitButton = transform.GetComponent<Button>();
+    }
+
+    public void ObjectToggle()
+    {
+        // target.SetActive(!target.activeInHierachy);
+    }
+}
